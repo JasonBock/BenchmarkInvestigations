14589a9febd621c00181ccfe3484fe21ddf3d311
Jason Bock <jason.r.bock@outlook.com>
2022-01-10 08:56:21 -0600

    #2 started gettings tests in place

---
+++ BenchmarkInvestigations.Core.Tests/ArgumentNullCheckOverheadTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace BenchmarkInvestigations.Core.Tests
+{
+	public static class ArgumentNullCheckOverheadTests
+	{
+		[Test]
+		public static void PassToNullCheck() => 
+			Assert.That(new ArgumentNullCheckOverhead().PassToNullCheck(), Is.Not.Null);
+
+		[Test]
+		public static void PassWithoutNullCheck() =>
+			Assert.That(new ArgumentNullCheckOverhead().PassWithoutNullCheck(), Is.Not.Null);
+	}
+}
