2a2041bf7f562823308bf2dc6f340a1059567746
Jason Bock <jason.r.bock@outlook.com>
2022-01-11 11:11:40 -0600

    Removed a null check in a test

--- BenchmarkInvestigations.Core.Tests/FormattingGuidsTests.cs
+++ BenchmarkInvestigations.Core.Tests/FormattingGuidsTests.cs
@@ -13,7 +13,6 @@ public static class FormattingGuidsTests
 
 		Assert.Multiple(() =>
 		{
-			Assert.That(formatResult, Is.Not.Null);
 			Assert.That(() => Guid.ParseExact(formatResult, "N"), Throws.Nothing);
 			Assert.That(formatResult, Is.EqualTo(replaceResult));
 		});
